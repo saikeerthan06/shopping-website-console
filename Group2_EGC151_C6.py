1b41e0626f4b7f9f8f4d6b081199ad9d7b2296be
Sai Keerthan <130973516+saikeerthan06@users.noreply.github.com>
2023-08-07 02:13:47 +0800

    Add files via upload
    newest versions with rectified authentication tokens for twilio api.

---
+++ Group2_EGC151_C6.py
@@ -0,0 +1,224 @@
+import re
+import datetime
+import sending_SMS as SS
+import sign_up as SU
+
+def get_time_of_day_greeting():
+    current_hour = datetime.datetime.now()
+    return current_hour.strftime("Greetings! Current Date and Time: %B %d, %Y %H:%M:%S")
+
+def welcome_page():
+    print("\n====== WELCOME TO GUNDAM STORE! ======")
+    print("1. View Membership details")
+    print("2. Enter Shopping Menu")
+    print("3. Show Shopping Cart")
+    print("4. Checkout")
+    print("5. Quit")
+    
+
+def homepage():
+    print("----------------------------")
+    greeting = get_time_of_day_greeting()
+    print(f"{greeting}")
+    print("---- Welcome To Gundam Store! ----")
+    print("1. Show Items For Sale ")
+    print("2. Check Shopping Cart")
+    print("3. Check Membership Status and checkout\n")
+
+
+global catalog_PG, Catalog_RG, Catalog_MG, Catalog_MG1, Catalog_M
+catalog_PG = {"Gundam Raiser": 315.0, "Gundam Seed Astray": 320.0, "Wing Gundam": 350.0, "Freedom Gundam": 410.0}
+Catalog_RG = {"RG GoldyMarg": 52.0, "RG Gao Gai Gar": 78.0, "God Gundam": 58.0, "Wing Gundam Astray": 38.0}
+Catalog_MG = {"Eclipse Gundam": 195.0, "Full Saber": 72.0, "Unicorn Gundam": 64.0, "Gundam Dynames": 56.0}
+Catalog_MG1 = {"Messiah Ranka Lee": 95.0, "Ganesa" : 20.0, "Arcanadea Lumitea": 75.0, "Tsubasa Kazanari": 90.0}
+Catalog_M = {"Little Ryan": 30.0, "Elephant Racer": 17.0, "Zoids Stylaser": 148.0,  "Cannon Bull": 35.0}
+
+global category_dictionaries
+category_dictionaries = [catalog_PG, Catalog_RG, Catalog_MG, Catalog_MG1, Catalog_M]
+
+def show_items_for_sale():
+    print("\n PRODUCTS FOR SALE")
+    print("---------------------------")
+    print("Categories")
+    print("---------------------")
+    print("1. Perfect Grade")
+    print("2. Real Grade")
+    print("3. Master Grade")
+    print("4. Mecha Girl")
+    print("5. Motorized")
+
+    global category_choice
+    category_choice = int(input("Please choose the desired Category Number: "))
+
+    if category_choice == 1:
+        print("\n==== Perfect Grade ====\n")
+
+        counter_PG = 1
+
+        print("Product\t\t\t|\t Cost")
+        print("---------------------------------------------")
+        for key, val in catalog_PG.items():
+            if key == "Wing Gundam":
+                print(f"{counter_PG}. {key}\t\t\t ${val}")
+            else:
+                print(f"{counter_PG}. {key}\t\t ${val}")
+            counter_PG += 1
+
+    elif category_choice == 2:
+        print("\n==== Real Grade ====\n")
+        counter_RG = 1
+
+        print("Product\t\t\t|\t Cost")
+        print("---------------------------------------------")
+        for key, val in Catalog_RG.items():
+            if key == "RG GoldyMarg" or key == "God Gundam":
+                print(f"{counter_RG}. {key}\t\t\t ${val}")
+            else:
+                print(f"{counter_RG}. {key}\t\t ${val}")
+            counter_RG += 1
+
+    elif category_choice == 3:
+        print("\n==== Master Grade ====\n")
+
+        counter_MG = 1
+
+        print("Product\t\t|\t Cost")
+        print("---------------------------------------------")
+        for key, val in Catalog_MG.items():
+            if key == "Full Saber":
+                print(f"{counter_MG}. {key}\t\t ${val}")
+            else:
+                print(f"{counter_MG}. {key}\t ${val}")
+            counter_MG += 1
+
+    elif category_choice == 4:
+        print("\n==== Mecha Girl ====")
+
+        counter_MG1 = 1
+
+        print("Product\t\t |\tCost")
+        print("---------------------------------------------")
+        for key, val in Catalog_MG1.items():
+            if key == "Ganesa":
+                print(f"{counter_MG1}. {key}\t\t ${val}")
+            else:
+                print(f"{counter_MG1}. {key}\t ${val}")
+            counter_MG1 += 1
+
+    elif category_choice == 5:
+        print("\n==== Motorised ====")
+
+        counter_M = 1
+
+        print("Product\t\t |\tCost")
+        print("---------------------------------------------")
+        for key, val in Catalog_M.items():
+            if key == "Little Ryan" or key == "Cannon Bull":
+                print(f"{counter_M}. {key}\t\t ${val}")
+            else:
+                print(f"{counter_M}. {key}\t ${val}")
+            counter_M += 1
+
+    item_choice = input("Please Select the item Number(s)(separated by commas): ")
+
+    selected_products = []  # empty list, nothing is added yet.
+
+    return item_choice, category_choice
+
+
+def item_choice_handling(item_choice):
+    item_choice_list = item_choice.split(",")
+    return item_choice_list
+
+
+def adding_item_to_cart(cart, item_choice_list, chosen_dict):
+    pdt_names = list(chosen_dict.keys())
+
+    for element in item_choice_list:
+        cart[pdt_names[int(element) - 1]] = chosen_dict[pdt_names[int(element) - 1]]
+
+    return cart
+
+
+def display_cart(cart):
+
+    print("Name\t\t          |\t Cost")
+    print("----------------------------------------")
+
+    if len(cart) != 0:
+        counter = 1
+        for key, value in cart.items():
+            if (key == "Full Saber") or (key == "Ganesa") or (key == "Wing Gundam") or (key == "RG GoldyMarg") or (key == "God Gundam") or (key == "Little Ryan") or (key == "Cannon Bull"):
+                print(f"{counter}. {key}\t\t\t {value}")
+            else:
+                print(f"{counter}. {key}\t\t {value}")
+            counter += 1
+    else:
+        print("Cart is Empty! Please add in items to Purchase!")
+
+
+def remove_item_cart(cart, pdt_removal_list):
+    if len(cart) != 0:
+        pdt_list = list(cart.keys())
+        for i in pdt_removal_list:
+            del cart[pdt_list[int(i) - 1]]
+    else:
+        print("Cart is empty! Unable to remove further!")
+
+def increase_quantity(cart, pdt_id, pdt_qty):
+    cart_item_names = list(cart.keys())
+    cart[cart_item_names[pdt_id - 1]] = cart[cart_item_names[pdt_id - 1]] * pdt_qty
+
+
+def decrease_quantity(cart, pdt_id, pdt_qty):
+    cart_item_names = list(cart.keys())
+
+    for category in category_dictionaries:
+        if cart_item_names[pdt_id-1] in category:
+            if cart[cart_item_names[pdt_id - 1]] > category[cart_item_names[pdt_id-1]]:
+                cart[cart_item_names[pdt_id - 1]] = cart[cart_item_names[pdt_id - 1]] / (pdt_qty + 1)
+            else:
+                remove_item_cart(cart, str(pdt_id))
+
+def calculate_total_amount(cart, membership_type):
+    #define membership type and corresponding discount
+    if membership_type == 'gold':
+        discount = 0.15
+    elif membership_type == 'silver':
+        discount= 0.10
+    elif membership_type == 'bronze':
+        discount = 0.05
+    elif membership_type == "nil":
+        discount = 1
+
+    #constant - therefore variable name in Capslock
+    GST = 0.08
+
+    #getting the sum of all the products in the cart
+    total_sum = sum(list(cart.values()))
+
+    #calculating the discounted amount
+    if membership_type != "nil":
+        discounted_amount = discount * total_sum
+    else:
+        discounted_amount = 0
+
+    discounted_price = total_sum - discounted_amount
+
+    #final price after GST
+    final_price = discounted_price + (GST * discounted_price)
+
+    return total_sum, discounted_amount, discounted_price, final_price
+
+
+
+
+
+
+
+
+
+    
+
+
+
---
+++ sign_up.py
@@ -0,0 +1,197 @@
+#SIGNING UP
+
+import re
+import datetime
+import sending_SMS as SS
+import Group2_EGC151_C6 as GS
+
+def is_valid_phone_number(phone_number):
+    return re.match(r"^\d{8}$", phone_number)
+
+def is_valid_email(email):
+    return re.match(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", email)
+
+def is_valid_birthdate(birthdate):
+    try:
+        year, month, day = map(int, birthdate.split('-'))
+        date_obj = datetime.date(year, month, day)
+        current_year = datetime.date.today().year
+
+        if year < 1900 or year > current_year:
+            return False
+
+        if month < 1 or month > 12:
+            return False
+
+        if day < 1 or day > 31:
+            return False
+
+        # Check for February 30 and February 31
+        if month == 2 and day > 29:
+            return False
+
+        # Check for months with 30 days
+        if month in [4, 6, 9, 11] and day > 30:
+            return False
+
+        return True
+
+    except ValueError:
+        return False
+
+
+
+users_info = {}  
+
+def sign_up_for_membership():
+    print("\n=== SIGN UP  ===")
+
+    #user specific ditionary 
+    new_user = {}
+
+    while True:
+        phone_number = input("Enter your phone number (or 'q' to quit): ")
+        if phone_number.lower() == 'q':
+            return False
+        if is_valid_phone_number(phone_number):
+            new_user['phone_num'] = phone_number
+            break
+        print("Invalid phone number format. Please enter a valid phone number.")
+
+    email = input("Enter your email (or 'q' to quit): ")
+    if email.lower() == 'q':
+        return False
+    while not is_valid_email(email):
+        print("Invalid email format. Please enter a valid email address.")
+        email = input("Enter your email (e.g., example@example.com, or 'q' to quit): ")
+        if email.lower() == 'q':
+            return False
+    else:
+        new_user['email'] = email
+
+    full_name = input("Enter your full name as on NRIC: ")
+    new_user['name'] = full_name
+
+    while True:
+        age_str = input("Enter your age: ")
+        if age_str.isdigit():
+            age = int(age_str)
+            new_user["age"] = age
+            break
+        else:
+            print("Invalid age. Please enter a valid number.")
+
+    while True:
+        birthdate = input("Enter your birthdate (YYYY-MM-DD, or 'q' to quit): ")
+        if birthdate.lower() == 'q':
+            new_user["birthdate"] = birthdate
+            return False
+        if is_valid_birthdate(birthdate):
+            break
+        print("Invalid birthdate format. Please enter a valid date in the format YYYY-MM-DD.")
+
+    while True:
+        credit_card = input("Please enter your 16 digit credit card number: ")
+        if len(credit_card) != 16:
+            print('ERROR: Please input a valid 16 digit Credit Card Number')
+        else: 
+            new_user['credit_card_num'] = credit_card
+            break
+
+    while True:
+        CVV_number = input("Please enter your CVV Number: ")
+        if len(CVV_number) != 3:
+            print("ERROR: Please input Valid CVV number")
+        else:
+            new_user['cvv_num'] = CVV_number
+            break
+
+    while True:
+        address = input("Please enter delivery address: ")
+
+        if type(address) != str:
+            print("Invalid Input! Please enter the correct Address: ")
+        else:
+            new_user['address'] = address
+            break
+
+
+    print("\n================ MEMBERSHIP DETAILS =====================\n")
+
+    print("Gold Membership: $125, 15% Discount at checkout")
+    print("Silver Membership: $90, 10% Discount at checkout")
+    print('Bronze Membership: $50, 5% Discount at checkout\n')
+
+    class_ask = input("Please Choose the Membership Class (eg: gold, silver, bronze) OR type 'nil' if you are not interested! : ").strip().lower()
+
+    if class_ask not in ["gold", "silver", "bronze", "nil"]:
+        print("ERROR: Please input a valid membership type")
+        return sign_up_for_membership(credit_card)
+    elif class_ask in ["gold", "silver", "bronze", "nil"]:
+        new_user['membership_type'] = class_ask
+        if class_ask == "gold":
+            pp_ask = input("You have selected the gold membership class, you will be charged $125. Please press 'y' to continue: ")
+            if pp_ask == "yes" or pp_ask == "y":
+                amt = 125
+                memb_id = '1234A'
+                users_info[CVV_number] = new_user
+       
+        elif class_ask == "silver":
+            pp_ask = input("You have selected the Silver membership class, you will be charged $90. Please press 'y' to continue: ")
+            if pp_ask == "yes" or pp_ask == "y":
+                #sending SMS in here
+                amt = 90
+                memb_id = '5678B'
+                users_info[CVV_number] = new_user
+        elif class_ask == "bronze":
+            pp_ask = input("You have selected the Bronze membership class, you will be charged $50. Please press 'y' to continue: ")
+            if pp_ask == "yes" or pp_ask == "y":
+                amt = 50
+                memb_id = '9101C'
+                users_info[CVV_number] = new_user
+    
+        elif class_ask == "nil":
+            users_info[CVV_number] = new_user
+
+    
+        if class_ask != "nil":
+            print(f"${amt} has been charged to credit card number ending with {credit_card[-4:]}!")
+            print(f"Here is your Membership ID: {memb_id}. Use this in Checkout for a 15% discount!")
+            SS.sending_confirmation(amt, credit_card, class_ask)
+            print(f"An SMS has already been sent with the details to your number. Thank You for choosing {class_ask} Membership!\n")
+
+
+
+
+#Viewing the Membership Details
+
+def view_membership():
+    ID_input = str(input("Please enter your CVV Number: "))
+
+    #1. check if CVV is valid
+    if len(ID_input) != 3:
+        print("Invalid Input! Please enter a Valid CVV number.")
+        return ID_input
+    else:
+        print("You can now view your Membership Information below: \n")
+        if users_info[ID_input]['membership_type'] == "nil":
+            print("You have not signed up for membership yet! Please Sign up at the Welcome Page if you are interested!")
+
+    #2. check if it is in users_info
+    if ID_input not in users_info:
+        print('ERROR: Please input proper ID Input')
+        return ID_input
+    
+   #3. iterate over the key-value item pairs together and display\
+    print("\n========= MEMBERSHIP DETAILS =============\n")
+    print(f"Name: {users_info[ID_input]['name']}")
+    print(f"Email: {users_info[ID_input]['email']}")
+    print(f"Age: {users_info[ID_input]['age']}")
+    print(f"Phone Number: {users_info[ID_input]['phone_num']}")
+    print(f"Membership Class: {users_info[ID_input]['membership_type']}\n\n")
+
+
+
+
+
+
